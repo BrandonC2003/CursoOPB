1fc87af448d829386c05e480b7583845fde8db2f
CASTRO CONTRERAS BRANDON ALEXANDER <2505022021@mail.utec.edu.sv>
2023-04-04 19:08:21 -0600

    primer commit

---
+++ cursoOpenBootcamp/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cursoOpenBootcamp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ingresa tu nombre: ");
+            string nombre = Console.ReadLine();
+
+            Console.WriteLine(nombre);
+
+            string hora = DateTime.Now.ToString("hh:mmm");
+            Console.WriteLine(hora);
+            Console.Read();
+        }
+    }
+}
